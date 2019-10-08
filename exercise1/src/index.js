77ec05a295850e48563729743550b4f9308f66fe
Pha Nguyen <sephanguyen@gmail.com>
2019-10-08 18:38:20 +0700

    update excersice 1

--- exercise1/src/index.js
+++ exercise1/src/index.js
@@ -21,10 +21,10 @@ function rotate2dKtime(grid, kRotation) {
 function rotate2d90degree(grid) {
   const n = grid.length;
   const newGrid = JSON.parse(JSON.stringify(grid));
-
+  let temp;
   for (let i = 0; i < n / 2; i++) {
     for (let j = i; j < n - i - 1; j++) {
-      let temp = newGrid[i][j];
+      temp = newGrid[i][j];
       newGrid[i][j] = newGrid[n - 1 - j][i];
       newGrid[n - 1 - j][i] = newGrid[n - 1 - i][n - 1 - j];
       newGrid[n - 1 - i][n - 1 - j] = newGrid[j][n - 1 - i];

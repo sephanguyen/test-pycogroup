f53239d38cd2a9b6921162a46a18f35ec17a4073
Pha Nguyen <sephanguyen@gmail.com>
2019-10-08 19:26:31 +0700

    update exercise 4

--- exercise4/src/index.js
+++ exercise4/src/index.js
@@ -1,17 +1,40 @@
-function isCanBooking(arrivals, departures, k) {
-  const n = arrivals.length;
+const MESSAGE_ROOM_INVALID = 'num room is invalid';
+const MESSAGE_ARRIVALS_INVALID = 'arrivals is invalid';
+const MESSAGE_DEPATURES_INVALID = 'departures is invalid';
+const MESSAGE_DEPATURES_AND_ARRIVALS_NOT_EQUAL_LENGTH_INVALID =
+  'length of arrivals and departures not equal';
+function isCanBooking(arrivals, departures, numRooms) {
+  if (!Number.isInteger(numRooms)) {
+    throw new Error(MESSAGE_ROOM_INVALID);
+  }
+  if (
+    !Array.isArray(arrivals) ||
+    !arrivals.every(arrival => Number.isInteger(arrival))
+  ) {
+    throw new Error(MESSAGE_ARRIVALS_INVALID);
+  }
+  if (
+    !Array.isArray(departures) ||
+    !departures.every(departure => Number.isInteger(departure))
+  ) {
+    throw new Error(MESSAGE_DEPATURES_INVALID);
+  }
+  if (departures.length != arrivals.length) {
+    throw new Error(MESSAGE_DEPATURES_AND_ARRIVALS_NOT_EQUAL_LENGTH_INVALID);
+  }
+  const numberArrival = arrivals.length;
   arrivals = arrivals.sort((a, b) => a - b);
   departures = departures.sort((a, b) => a - b);
 
   let count = 0;
   let indexArrival = 0;
   let indexDeparture = 0;
-  while (indexArrival < n && indexDeparture < n) {
+  while (indexArrival < numberArrival && indexDeparture < numberArrival) {
     if (arrivals[indexArrival] < departures[indexDeparture]) {
       indexArrival++;
       count++;
 
-      if (count > k) {
+      if (count > numRooms) {
         return false;
       }
     } else {
@@ -23,4 +46,10 @@ function isCanBooking(arrivals, departures, k) {
   return true;
 }
 
-export { isCanBooking };
+export {
+  isCanBooking,
+  MESSAGE_ROOM_INVALID,
+  MESSAGE_ARRIVALS_INVALID,
+  MESSAGE_DEPATURES_INVALID,
+  MESSAGE_DEPATURES_AND_ARRIVALS_NOT_EQUAL_LENGTH_INVALID
+};
--- exercise4/test/index.test.js
+++ exercise4/test/index.test.js
@@ -1,16 +1,77 @@
 import { expect } from 'chai';
-import { isCanBooking } from '../src/index';
+import {
+  isCanBooking,
+  MESSAGE_ROOM_INVALID,
+  MESSAGE_ARRIVALS_INVALID,
+  MESSAGE_DEPATURES_INVALID,
+  MESSAGE_DEPATURES_AND_ARRIVALS_NOT_EQUAL_LENGTH_INVALID
+} from '../src/index';
 describe('isCanBooking', function() {
   it('not booking when has one room', () => {
     const arrivals = [1, 3, 5];
     const departures = [2, 6, 10];
-    const k = 1;
-    expect(isCanBooking(arrivals, departures, k)).equal(false);
+    const numberRoom = 1;
+    expect(isCanBooking(arrivals, departures, numberRoom)).equal(false);
   });
   it('not booking when has two room', () => {
     const arrivals = [1, 3, 5];
     const departures = [2, 6, 10];
-    const k = 2;
-    expect(isCanBooking(arrivals, departures, k)).equal(true);
+    const numberRoom = 2;
+    expect(isCanBooking(arrivals, departures, numberRoom)).equal(true);
+  });
+
+  it('Throw exception when numberRoom is invalid', () => {
+    const arrivals = [1, 3, 5];
+    const departures = [2, 6, 10];
+    const numberRoom = '2';
+    expect(() => isCanBooking(arrivals, departures, numberRoom)).to.throws(
+      Error,
+      MESSAGE_ROOM_INVALID
+    );
+  });
+  it('Throw exception when arrivals is not array', () => {
+    const arrivals = 1;
+    const departures = [2, 6, 10];
+    const numberRoom = 2;
+    expect(() => isCanBooking(arrivals, departures, numberRoom)).to.throws(
+      Error,
+      MESSAGE_ARRIVALS_INVALID
+    );
+  });
+  it('Throw exception when arrivals is array invalid', () => {
+    const arrivals = [2, 'asdsa', 10];
+    const departures = [2, 6, 10];
+    const numberRoom = 2;
+    expect(() => isCanBooking(arrivals, departures, numberRoom)).to.throws(
+      Error,
+      MESSAGE_ARRIVALS_INVALID
+    );
+  });
+  it('Throw exception when departures is array invalid', () => {
+    const arrivals = [1, 3, 5];
+    const departures = [2, 'zxy', 10];
+    const numberRoom = 2;
+    expect(() => isCanBooking(arrivals, departures, numberRoom)).to.throws(
+      Error,
+      MESSAGE_DEPATURES_INVALID
+    );
+  });
+  it('Throw exception when departures is not array', () => {
+    const arrivals = [1, 3, 5];
+    const departures = '1';
+    const numberRoom = 2;
+    expect(() => isCanBooking(arrivals, departures, numberRoom)).to.throws(
+      Error,
+      MESSAGE_DEPATURES_INVALID
+    );
+  });
+  it('Throw exception when length departures and arrivals not equal', () => {
+    const arrivals = [1, 3, 5, 1];
+    const departures = [2, 6, 10];
+    const numberRoom = 2;
+    expect(() => isCanBooking(arrivals, departures, numberRoom)).to.throws(
+      Error,
+      MESSAGE_DEPATURES_AND_ARRIVALS_NOT_EQUAL_LENGTH_INVALID
+    );
   });
 });
